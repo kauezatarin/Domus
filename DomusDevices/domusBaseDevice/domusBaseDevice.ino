a1468e6cd732bf3ea5051ca72a38854bd1fd303f
Kauê Zatarin <kauezatarin@hotmail.com>
2018-07-11 01:50:02 -0300

    Changed Server Assembly Version.

--- DomusDevices/domusBaseDevice/domusBaseDevice.ino
+++ DomusDevices/domusBaseDevice/domusBaseDevice.ino
@@ -3,6 +3,7 @@
 #include <DHT.h>
 #include <DHT_U.h>
 #include <EEPROM.h>
+#include <Arduino.h>  // for type definitions
 
 #define DHTPIN A5 // pino que estamos conectado
 #define DHTTYPE DHT11 // DHT 11
@@ -232,4 +233,21 @@ void EEPROM_Clear() {
   return;
 }
 
+template <class T> int EEPROM_writeAnything(int ee, const T& value)
+{
+    const byte* p = (const byte*)(const void*)&value;
+    unsigned int i;
+    for (i = 0; i < sizeof(value); i++)
+          EEPROM.write(ee++, *p++);
+    return i;
+}
+
+template <class T> int EEPROM_readAnything(int ee, T& value)
+{
+    byte* p = (byte*)(void*)&value;
+    unsigned int i;
+    for (i = 0; i < sizeof(value); i++)
+          *p++ = EEPROM.read(ee++);
+    return i;
+}
 

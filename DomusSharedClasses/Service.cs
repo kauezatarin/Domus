bd589adb63fa72e45318acf7c7b0b5d935483121
Kauê Zatarin <kauezatarin@hotmail.com>
2018-09-28 20:21:33 -0300

    Criada classe e tabela de serviços disponiveis no sistema

---
+++ DomusSharedClasses/Service.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+
+namespace DomusSharedClasses
+{
+    [Serializable]
+    public class Service
+    {
+        public Service(int serviceId, string serviceName, string deviceId, int devicePortNumber)
+        {
+            ServiceId = serviceId;
+            ServiceName = serviceName;
+            DeviceId = deviceId;
+            DevicePortNumber = devicePortNumber;
+        }
+
+        public int ServiceId { get; set; }
+
+        public string ServiceName { get; set; }
+
+        public string DeviceId { get; set; }
+
+        public int DevicePortNumber { get; set; }
+
+    }
+}

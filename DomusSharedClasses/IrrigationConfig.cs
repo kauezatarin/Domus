84626e60f9836cb1d0f4335504347c20a6724841
Kauê Zatarin <kauezatarin@hotmail.com>
2018-10-02 20:21:32 -0300

    Implementada a classe IrrigationConfig

--- DomusSharedClasses/IrrigationConfig.cs
+++ DomusSharedClasses/IrrigationConfig.cs
@@ -8,5 +8,24 @@ namespace DomusSharedClasses
 
     class IrrigationConfig
     {
+        public IrrigationConfig(int configId, int maxSoilHumidity, int minAirTemperature, int maxAirTemperature, bool useForecast)
+        {
+            ConfigId = configId;
+            MaxSoilHumidity = maxSoilHumidity;
+            MinAirTemperature = minAirTemperature;
+            MaxAirTemperature = maxAirTemperature;
+            UseForecast = useForecast;
+        }
+
+        public int ConfigId { get; set; }
+
+        public int MaxSoilHumidity { get; set; }
+
+        public int MinAirTemperature { get; set; }
+
+        public int MaxAirTemperature { get; set; }
+
+        public bool UseForecast { get; set; }
+
     }
 }
